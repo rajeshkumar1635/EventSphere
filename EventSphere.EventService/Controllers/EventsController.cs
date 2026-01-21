e7e72db27c10442e88f091aceda77135ca590fa3
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2026-01-20 19:27:40 -0500

    secure event service api with jwt via api gateway

---
+++ EventSphere.EventService/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using EventSphere.EventService.Data;
+using EventSphere.EventService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSphere.EventService.Controllers
+{
+    [ApiController]
+    [Route("api/events")]
+    public class EventsController : ControllerBase
+    {
+        private readonly EventDbContext _context;
+
+        public EventsController(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetEvents()
+        {
+            var events = await _context.Events.ToListAsync();
+            return Ok(events);
+        }
+        [HttpPost]
+        [Authorize(Roles ="Admin,Organizer")]
+        public async Task<IActionResult> CreateEvent(Event evt)
+        {
+            _context.Events.Add(evt);
+            await _context.SaveChangesAsync();
+            return Ok(evt);
+        }
+    }
+}
