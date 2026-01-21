a2a715b8e6f5f2172ebcdbf1e3714e533f052012
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2026-01-20 20:13:36 -0500

    Added Update and Delete functionality by the admin

--- EventSphere.EventService/Controllers/EventsController.cs
+++ EventSphere.EventService/Controllers/EventsController.cs
@@ -33,5 +33,33 @@ namespace EventSphere.EventService.Controllers
             await _context.SaveChangesAsync();
             return Ok(evt);
         }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles ="Admin,Organizer")]
+        public async Task<IActionResult> UpdateEvent(int id,Event updatedEvent)
+        {
+            var evt = await _context.Events.FindAsync(id);
+            if (evt == null)
+                return NotFound();
+            evt.Title = updatedEvent.Title;
+            evt.Description = updatedEvent.Description;
+            evt.EventDate = updatedEvent.EventDate;
+            evt.Location = updatedEvent.Location;
+
+            await _context.SaveChangesAsync();
+            return Ok(evt);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            var evt = await _context.Events.FindAsync(id);
+            if (evt == null)
+                return NotFound();
+            _context.Events.Remove(evt);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
